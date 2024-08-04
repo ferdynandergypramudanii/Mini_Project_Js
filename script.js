a58a545c3e5d622b42b3f41b6c75cec4ead8e2e5
ferdynand <ferdynandergypramudani11@gmail.com>
2024-08-04 11:03:35 +0700

    addEventListener

--- script.js
+++ script.js
@@ -10,7 +10,16 @@ function addTask() {
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
-        li.appendChild(span)
+        li.appendChild(span);
     }
     inputBox.value = "";
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    } 
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
