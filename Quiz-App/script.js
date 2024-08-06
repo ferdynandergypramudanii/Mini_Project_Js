7331a3d7e4e7410220c899c15691df62b868754c
ferdynand <ferdynandergypramudani11@gmail.com>
2024-08-06 22:32:07 +0700

    add array of object

---
+++ Quiz-App/script.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "Which is larger animal in the world ?",
+        answer: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is larger animal in the world ?",
+        answer: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is largest desert in the world ?",
+        answer: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: true},
+            {text: "Sahara", correct: false},
+            {text: "Antartica", correct: true},
+        ]
+    },
+    {
+        question: "Which is larger the smallest continent in the world ?",
+        answer: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
+        ]
+    },
+];
